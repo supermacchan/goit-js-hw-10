c6b11661033f52e915bafca2e61eb3e2d3b67d9f
supermacchan <aiba.aki9@gmail.com>
2022-10-06 18:46:48 +0000

    Deploying to gh-pages from @ supermacchan/goit-js-hw-10@544b967f28aa8398953e8c68485513a233612f45 🚀

---
+++ index.14da40b3.js
@@ -0,0 +1,2 @@
+!function(){function n(n){fetch("https://restcountries.com/v3.1/name/".concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}))}document.querySelector("#search-box").addEventListener("input",(function(t){n(t.target.value)}))}();
+//# sourceMappingURL=index.14da40b3.js.map
