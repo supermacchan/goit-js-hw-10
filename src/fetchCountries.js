544b967f28aa8398953e8c68485513a233612f45
Olga Birkovska <birkovskaya92@ukr.net>
2022-10-06 21:46:23 +0300

    update task 1

---
+++ src/fetchCountries.js
@@ -0,0 +1,18 @@
+function fetchCountries(name) {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+export { fetchCountries };
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetchCountries("Ukraine");
+
+const inputEl = document.querySelector('#search-box');
+
+inputEl.addEventListener('input', onSearchInput);
+
+function onSearchInput(e) {
+    const name = e.target.value;
+    fetchCountries(name);
+}
