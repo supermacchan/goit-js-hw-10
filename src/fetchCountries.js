5c55dcec3597f2decf1d8c05a5497ce6fad546f7
Olga Birkovska <birkovskaya92@ukr.net>
2022-10-07 21:21:02 +0300

    update task 1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,7 @@
 import Notiflix from 'notiflix';
 
 const countryCard = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 function fetchCountries(name) {
     fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
@@ -11,28 +12,55 @@ function fetchCountries(name) {
             return response.json();
         })
         .then(data => {
-            console.log(data[0]);
-            const markup = generateMarkup(data[0]);
-            console.log(markup);
-            countryCard.innerHTML = markup;
+            if (data.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+                return;
+            } else if (data.length >= 2 && data.length <= 10) {
+                console.log(data);
+                const newList = generateCountryList(data);
+                countryList.innerHTML = newList;
+                countryCard.innerHTML = '';
+            } else if (data.length === 1) {
+                console.log(data[0]);
+                const newMarkup = generateCountryMarkup(data[0]);
+                console.log(newMarkup);
+                countryCard.innerHTML = newMarkup;
+                countryList.innerHTML = '';
+            }  
         })
         .catch(error => {
             console.log(error);
             Notiflix.Notify.failure("Oops, there is no country with that name");
+            countryList.innerHTML = '';
+            countryCard.innerHTML = '';
         })
 }
 
-function generateMarkup(country) {
+function generateCountryMarkup(country) {
     return `<h2 class="country-name">
                 <img class="country-flag" src="${country.flags.svg}" alt="${country.name.official}" width="40" height="25" />
                 ${country.name.official}
             </h2>
             <ul class="country-properties">
-                <li class="country-capital">Capital: ${country.capital}</li>
-                <li class="country-population">Population: ${country.population}</li>
-                <li class="country-langs">Languages: ${Object.values(country.languages)}</li>
+                <p class="country-capital"><b>Capital:</b> ${country.capital}</p>
+                <p class="country-population"><b>Population:</b> ${country.population}</p>
+                <p class="country-langs"><b>Languages:</b> ${Object.values(country.languages).join(', ')}</p>
             </ul>`
 }
+
+function generateCountryList(countries) {
+    result = countries.map(country => {
+        const flag = country.flags.svg;
+        const name = country.name.official;
+        return `<div><p class="country-name">
+                <img class="country-flag" src="${flag}" alt="${name}" width="40" height="25" />
+                ${name}
+                </p></div>`
+        
+    })
+    return result.join('');
+}
+
 
 export { fetchCountries };
 
