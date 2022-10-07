6b7330b71e2f9961a2ca8c14f705a63020fa072c
Olga Birkovska <birkovskaya92@ukr.net>
2022-10-07 21:34:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
+const countryCard = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 inputEl.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 
 function onSearchInput(e) {
     const name = e.target.value.trim();
     if (name === "") {
+        countryList.innerHTML = '';
+        countryCard.innerHTML = '';
         return;
     }
     fetchCountries(name);
